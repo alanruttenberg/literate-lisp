8c11b13a2f990796e4b5f747f123575184e56259
Alan Ruttenberg <alanruttenberg@gmail.com>
2019-11-26 15:31:10 -0500

    Checkpoint - starting to replace stale code. Updated ros-test to compiles as well. Reverted to unix diff as quicklisp diff didn't work in a number of implementations. Tested: abcl-bin cmu-bin ccl-bin sbcl sbcl-bin ECL. Known not to work: Allegro where we need to do something like with ABCL

--- t/ros-tests.sh
+++ t/ros-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd `dirname $0`
-ros run -e '(pushnew :literate-test *features*)'\
-    -e '(setq *load-verbose* t)'\
+ros --quiet run -e '(pushnew :literate-test *features*)'\
     -e '(load "../literate-lisp.asd")'\
     -e '(ql:quickload :fiveam)'\
     -e '(asdf::oos (quote asdf:load-op) :literate-lisp)'\
+    -e '(push :literate-test *features*)'\
+    -e '(compile-file "../literate-lisp.org")'\
     -e '(load "../literate-lisp.org")'\
     -e '(if (not (literate-lisp::run-test))(uiop:quit 1))'\
     -e '(uiop:quit 0)'
